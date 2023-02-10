691b2ba83e559df56831c196b1d8fe1c678b3bd3
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-10 11:06:04 +1100

    Create model.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
-mongoose.connect("mongodb://127.0.0.1/farmStand", {
+mongoose.connect("mongodb://127.0.0.1/yelp-camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -19,6 +20,15 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "cheap camping",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
