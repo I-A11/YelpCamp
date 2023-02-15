101e901aa821dc05dea8410cdb7cf9c2d73293f9
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-15 12:50:30 +1100

    Install ejs-mate to add layout.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://127.0.0.1/yelp-camp", {
@@ -16,6 +17,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
