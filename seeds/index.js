df477996bfc0502fa164aaa182659024358868f5
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-20 10:25:24 +1100

    Add images to campgrounds sites.

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: " https://picsum.photos/500/400",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis est odit quia, temporibus modi neque accusamus enim. Laborum cumque doloribus reprehenderit consequuntur delectus dolor recusandae laboriosam. Distinctio neque laborum labore?",
+      price,
     });
     await camp.save();
   }
