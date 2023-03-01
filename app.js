1ba684a89707cad6396812610d20b5c780a69b5d
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-03-02 10:45:17 +1100

    Create error page template.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 
@@ -43,6 +44,8 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError("Invalid campground data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -86,8 +89,14 @@ app.delete(
   })
 );
 
+app.all("*", (req, res, next) => {
+  next(new ExpressError("PAGE NOT FOUND", 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send("Oh boy there is an error");
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something went wrong1!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
